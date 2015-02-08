2617f5188e74e8be97a5ca33eb368e70b7f27aa0
Peter Schuller <peter.schuller@infidyne.com>
2015-02-08 12:03:30 -0800

    setup_env: Improve support for subdirectories among dotfiles.
    Bug fixed (would "ensure" source dir instead of target dir), and we
    now apply dottification to both directories and files - but in both
    cases only for the top-level.

--- setup_env.py
+++ setup_env.py
@@ -17,13 +17,15 @@ import sys
 
 log = logging.getLogger('scode/rcfiles')
 
-# subdirectory relative to rcfiles in which to look for dotfiles to install
+# Subdirectory relative to rcfiles in which to look for dotfiles to install.
 DOTFILES_SUBDIR = 'dotfiles'
 
-# regular expression that filenames must match in order to be picked up (not applied to directories)
+# Regular expression that filenames must match in order to be picked up (not applied to directories).
+# The primary purpose of this is to avoid picking up manually created garbage like editor backup
+# files.
 DOTFILES_RE = re.compile('^[0-9a-zA-Z-.]*$')
 
-# enable/disable side-effects to easy debugging
+# Enable/disable side-effects to ease debugging during development.
 DRY_RUN = False
 
 def ensure_symlink_installed(source_path, dest_path):
@@ -61,26 +63,47 @@ def ensure_directory_exists(path):
 
     return True
 
-def install_dotfiles(rcfiles_home, user_home):
+def install_dotfiles(dotfile_dir, user_home):
+    """
+    Install files starting at rcfiles_home into user_home by creating symbolic links.
+
+    For top-level files in dotfiles_base, the symbolic link name will have a dot prepended
+    to it. For files in sub-directories, the files are symlinked with an identical name. For
+    example, given this input:
+
+      zshrc
+      emacs.d/init.el
+
+    The sumlinks created will be:
+
+      .zshrc -> $dotfiles_base/zshrc
+      .emacs.d/init.el -> $dotfiles_base/emacs.d/init.el
+
+    The purpose of this behavior (special-casing the top-level) is to avoid having to
+    track files beginning with a dot in the source directory to avoid confusion.
+    """
+    def dottify(p):
+        parent, child = os.path.split(p)
+        return os.path.join(parent, '.' + child)
+    assert dottify('/home/user/zshrc') == '/home/user/.zshrc'
+
     def recur(source_dir, target_dir):
         for base_name in os.listdir(source_dir):
             source_path = os.path.join(source_dir, base_name)
-            dest_path = os.path.join(target_dir, base_name)
+            if source_dir == dotfile_dir:
+                dest_path = dottify(os.path.join(target_dir, base_name))
+            else:
+                dest_path = os.path.join(target_dir, base_name)
 
             if os.path.isdir(source_path):
-                if ensure_directory_exists(source_path):
+                if ensure_directory_exists(dest_path):
                     recur(source_path, dest_path)
             else:
                 if not DOTFILES_RE.match(base_name):
                     log.warn('WRN skipping because of filename: %s', source_path)
                     continue
-                # dot files don't have a leading "." in the source, for human purposes so mangle
-                # the destination path to have one.
-                dotfile_path = os.path.join(os.path.split(dest_path)[0],
-                                            '.' + os.path.split(source_path)[1],)
-                ensure_symlink_installed(source_path, dotfile_path)
+                ensure_symlink_installed(source_path, dest_path)
 
-    dotfile_dir = os.path.join(rcfiles_home, DOTFILES_SUBDIR)
     recur(dotfile_dir, user_home)
 
 def setup_env(rcfiles_home, user_home):
@@ -91,12 +114,13 @@ def main():
 
     rel_rcfiles_home, _ = os.path.split(sys.argv[0])
     rcfiles_home = os.path.abspath(rel_rcfiles_home)
+    dotfiles_dir = os.path.join(rcfiles_home, DOTFILES_SUBDIR)
     user_home = os.path.expanduser("~")
 
     log.debug('rcfiles home: %s', rcfiles_home)
     log.debug('user home:    %s', user_home)
 
-    setup_env(rcfiles_home, user_home)
+    setup_env(dotfiles_dir, user_home)
 
 if __name__ == '__main__':
     main()
