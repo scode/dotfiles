83f8ffc0cca1a6cf6ab592af24937e3e15b3c506
Peter Schuller <peter.schuller@infidyne.com>
2015-08-28 22:57:29 -0700

    Only symlink top-level dotfiles/files.
    Rather than recursively symlinking, we now only symlink the top-level
    of ./dotfiles or ./files into the user's home.
    
    Other than simplifying this script, it leads to much trivial
    normalization:
    
      - Adding files doesn't require re-running setup.
      - Removing files doesn't require manual clean-up, since git
        does it for us as a result of us symlinking directly into
        the git repo hierarchy.
    
    Manul removal of old data installed is required on this update.

--- setup_env.py
+++ setup_env.py
@@ -17,13 +17,8 @@ import sys
 
 log = logging.getLogger('scode/rcfiles')
 
-# Subdirectory relative to rcfiles in which to look for dotfiles to install.
 DOTFILES_SUBDIR = 'dotfiles'
-
-# Regular expression that filenames must match in order to be picked up (not applied to directories).
-# The primary purpose of this is to avoid picking up manually created garbage like editor backup
-# files.
-DOTFILES_RE = re.compile('^[0-9a-zA-Z-.]*$')
+FILES_SUBDIR = 'files'
 
 # Enable/disable side-effects to ease debugging during development.
 DRY_RUN = False
@@ -46,81 +41,25 @@ def ensure_symlink_installed(source_path, dest_path):
         if not DRY_RUN:
             os.symlink(source_path, dest_path)
 
-def ensure_directory_exists(path):
-    """
-    @return True on full success, false if path exists but is not a directory and should be skipped.
-    """
-    if os.path.exists(path) and not os.path.isdir(path):
-        log.warn('WRN path exists but is not a directory (skipping this + all children): %s', path)
-        return False
+def install_symlinks(src_dir, dst_dir, prefix=''):
+    for src_name in os.listdir(src_dir):
+        src_path = os.path.join(src_dir, src_name)
+        dst_path = os.path.join(dst_dir, prefix + src_name)
 
-    if os.path.exists(path):
-        log.debug('OK  %s', path)
-    else:
-        log.info('MKD %s', path)
-        if not DRY_RUN:
-            os.makedirs(path)
-
-    return True
-
-def install_dotfiles(dotfile_dir, user_home):
-    """
-    Install files starting at rcfiles_home into user_home by creating symbolic links.
-
-    For top-level files in dotfiles_base, the symbolic link name will have a dot prepended
-    to it. For files in sub-directories, the files are symlinked with an identical name. For
-    example, given this input:
-
-      zshrc
-      emacs.d/init.el
-
-    The sumlinks created will be:
-
-      .zshrc -> $dotfiles_base/zshrc
-      .emacs.d/init.el -> $dotfiles_base/emacs.d/init.el
-
-    The purpose of this behavior (special-casing the top-level) is to avoid having to
-    track files beginning with a dot in the source directory to avoid confusion.
-    """
-    def dottify(p):
-        parent, child = os.path.split(p)
-        return os.path.join(parent, '.' + child)
-    assert dottify('/home/user/zshrc') == '/home/user/.zshrc'
-
-    def recur(source_dir, target_dir):
-        for base_name in os.listdir(source_dir):
-            source_path = os.path.join(source_dir, base_name)
-            if source_dir == dotfile_dir:
-                dest_path = dottify(os.path.join(target_dir, base_name))
-            else:
-                dest_path = os.path.join(target_dir, base_name)
-
-            if os.path.isdir(source_path):
-                if ensure_directory_exists(dest_path):
-                    recur(source_path, dest_path)
-            else:
-                if not DOTFILES_RE.match(base_name):
-                    log.warn('WRN skipping because of filename: %s', source_path)
-                    continue
-                ensure_symlink_installed(source_path, dest_path)
-
-    recur(dotfile_dir, user_home)
-
-def setup_env(rcfiles_home, user_home):
-    install_dotfiles(rcfiles_home, user_home)
+        ensure_symlink_installed(src_path,dst_path)
 
 def main():
     logging.basicConfig(level=logging.INFO)
 
     rel_rcfiles_home, _ = os.path.split(sys.argv[0])
     rcfiles_home = os.path.abspath(rel_rcfiles_home)
-    dotfiles_dir = os.path.join(rcfiles_home, DOTFILES_SUBDIR)
     user_home = os.path.expanduser("~")
 
     log.debug('rcfiles home: %s', rcfiles_home)
     log.debug('user home:    %s', user_home)
 
-    setup_env(dotfiles_dir, user_home)
+    install_symlinks(os.path.join(rcfiles_home, FILES_SUBDIR), user_home)
+    install_symlinks(os.path.join(rcfiles_home, DOTFILES_SUBDIR), user_home, prefix='.')
 
 if __name__ == '__main__':
     main()
