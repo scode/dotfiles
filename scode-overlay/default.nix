e80c3c4d5dce46366c36fd580a45abb745a2aece
Peter Schuller <peter.schuller@infidyne.com>
2018-01-24 00:02:04 -0800

    nix overlay - support installing a script from a file in the tree

--- scode-overlay/default.nix
+++ scode-overlay/default.nix
@@ -1,15 +1,50 @@
+# Install overlay by symlinking the containing directory as  ~/.config/nixpkgs/overlays/scode
+# and for initial install run nix-env -f '<nixpkgs>' -iA userPackages
+#
+# Note that nix-rebuild-scode will remove any packages not part of userPackages.
+
 self: super:
 
 {
-  # Install overlay by symlinking the containing directory as  ~/.config/nixpkgs/overlays/scode
-  # and for initial install run nix-env -f '<nixpkgs>' -iA userPackages
+
+  # Install a single-file executable script contained in this overlay
+  # tree itself. It's basically writeScriptBin except allows us to put
+  # the file next to this .nix file.
   #
-  # Note that nix-rebuild-scode will remove any packages not part of userPackages.
+  # Note: This does zero magic to pull in appropriate dependencies, rewrite
+  # shebangs, etc. It's merely a helper to install a file, period.
+  #
+  # (Should find a cleaner way of doing this, probably by creating a
+  # fetchFromTree (ala fetchFromGitHub) if/when I figure out how to do
+  # so correctly.)
+  scriptFromTree =
+    {
+      name,
+      treePath
+    }:
+    super.stdenv.mkDerivation rec {
+      inherit name;
+      phases = [ "buildPhase" ];
+
+      buildPhase = ''
+        mkdir -p $out/bin
+        echo cp ${treePath} $out/bin/${name}
+        cp ${treePath} $out/bin/${name}
+      '';
+    };
+
   userPackages = super.userPackages or {} // {
     # Must not forget nix itself or nix-env will disappear when nix-rebuild-scode is called.
     nix = super.nix;
 
     ripgrep = super.ripgrep;
+
+    # Demonstration use of scriptFromTree:
+    #
+    #scode-helloworld = self.scriptFromTree {
+    #  name = "scode-helloworld";
+    #  treePath = "${./scode-helloworld.py}";
+    #};
 
     nix-rebuild-scode = super.writeScriptBin "nix-rebuild-scode"
       ''
